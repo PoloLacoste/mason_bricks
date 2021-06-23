9d9af4a08ea0c003842e2135dc5672fb33984dda
JM REMEUR <jm@remeur.fr>
2021-06-23 22:26:46 +0200

    removed buildcontext

--- bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_bloc.dart
+++ bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_bloc.dart
@@ -6,11 +6,7 @@ import 'package:equatable/equatable.dart';
 part '{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Bloc extends Bloc<{{#pascalCase}}{{name}}{{/pascalCase}}Event, {{#pascalCase}}{{name}}{{/pascalCase}}State> {
-  {{#pascalCase}}{{name}}{{/pascalCase}}Bloc({
-    required this.context,
-  }) : super({{#pascalCase}}{{name}}{{/pascalCase}}Initial());
-
-  final BuildContext context;
+  {{#pascalCase}}{{name}}{{/pascalCase}}Bloc() : super({{#pascalCase}}{{name}}{{/pascalCase}}Initial());
 
   @override
   Stream<{{#pascalCase}}{{name}}{{/pascalCase}}State> mapEventToState(
--- bricks/cubit_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart
+++ bricks/cubit_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart
@@ -6,9 +6,5 @@ import 'package:equatable/equatable.dart';
 part '{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Cubit extends Cubit<{{#pascalCase}}{{name}}{{/pascalCase}}State> {
-  {{#pascalCase}}{{name}}{{/pascalCase}}Cubit({
-    required this.context,
-  }) : super({{#pascalCase}}{{name}}{{/pascalCase}}Initial());
-
-  final BuildContext context;
+  {{#pascalCase}}{{name}}{{/pascalCase}}Cubit() : super({{#pascalCase}}{{name}}{{/pascalCase}}Initial());
 }
