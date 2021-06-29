31040af6ba5be9506bfbf198d53cec2c6b6eb622
JM REMEUR <jm@remeur.fr>
2021-06-29 18:03:12 +0200

    remove immutable

--- bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_event.dart
+++ bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_event.dart
@@ -1,6 +1,5 @@
 part of '{{#snakeCase}}{{name}}{{/snakeCase}}_bloc.dart';
 
-@immutable
 abstract class {{#pascalCase}}{{name}}{{/pascalCase}}Event extends Equatable {
   const {{#pascalCase}}{{name}}{{/pascalCase}}Event();
 
