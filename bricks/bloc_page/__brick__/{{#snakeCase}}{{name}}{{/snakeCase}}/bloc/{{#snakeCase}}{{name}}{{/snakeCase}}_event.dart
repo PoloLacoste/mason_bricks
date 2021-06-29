4c2d1f1fb94d86e0e144571161fd2a2c718ed40e
JM REMEUR <jm@remeur.fr>
2021-06-29 17:58:37 +0200

    bloc event equatable

--- bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_event.dart
+++ bricks/bloc_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}_event.dart
@@ -1,4 +1,9 @@
 part of '{{#snakeCase}}{{name}}{{/snakeCase}}_bloc.dart';
 
 @immutable
-abstract class {{#pascalCase}}{{name}}{{/pascalCase}}Event {}
+abstract class {{#pascalCase}}{{name}}{{/pascalCase}}Event extends Equatable {
+  const {{#pascalCase}}{{name}}{{/pascalCase}}Event();
+
+  @override
+  List<Object> get props => [];
+}
